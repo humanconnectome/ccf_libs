a85d04d733244b297e36a4d74021f977bcfd0a8d
Moises Baltazar <me@moisesb.com>
2020-03-02 11:27:56 -0600

    Calculate interview_age for single study

--- redcap.py
+++ redcap.py
@@ -1,3 +1,4 @@
+import numpy as np
 import io
 import pandas as pd
 import requests
@@ -104,3 +105,38 @@ def get_behavioral_ids(keep_parents=False):
 
     return pd.concat(dfs, sort=False, ignore_index=True)
 
+def get_behavioral(study, fields=None):
+    if study not in config['behavioral']:
+        #throw Exception('This study is not available. ' + study)
+        print('Error', 'This study is not available.', study)
+        return {}
+
+    s = config['behavioral'][study]
+    fieldnames = s['fields']
+    events = s['events']
+    table = RedcapTable(s['token'])
+    fields = fields if fields else []
+    fields += list(fieldnames.values())
+    df = table.get_frame(fields, events)
+    df.rename(columns={
+        fieldnames['interview_date']: 'interview_date',
+        fieldnames['field']: 'subjectid'
+    }, inplace=True)
+    df = df[df.subjectid.notna() & (df.subjectid != '')]
+    split_df = df.subjectid.str.split("_", 1, expand=True)
+    df['subject'] = split_df[0].str.strip()
+    df['flagged'] = split_df[1].str.strip()
+    df['study'] = study
+
+    interview_date = pd.to_datetime(df.interview_date)
+    dob = pd.to_datetime(df.dob)
+    # interview age (in months, capped at 90 y.o.)
+    interview_age = (interview_date - dob) / np.timedelta64(1, 'M')
+    interview_age = interview_age.apply(np.floor).astype('Int64')
+    interview_age = interview_age.mask(interview_age > 1080, 1200)
+    df['interview_age'] = interview_age
+
+    if 'gender' in df.columns:
+        df.gender = df.gender.replace({1: 'M', 2: 'F'})
+
+    return df
