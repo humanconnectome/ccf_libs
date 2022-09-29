2f1b76bca4622f2da837b22b89f3dbcd19f515f9
Moises Baltazar <me@moisesb.com>
2022-09-29 09:53:09 -0400

    Upgrade dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from io import open
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
@@ -20,21 +20,18 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='ccf',  # Required
-
+    name="ccf",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.6',  # Required
-
+    version="0.4.7",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='A library of functions used by CCF',  # Optional
-
+    description="A library of functions used by CCF",  # Optional
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
     #
@@ -44,7 +41,6 @@ setup(
     # This field corresponds to the "Description" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-optional
     long_description=long_description,  # Optional
-
     # Denotes that our long_description is in Markdown; valid values are
     # text/plain, text/x-rst, and text/markdown
     #
@@ -55,22 +51,17 @@ setup(
     #
     # This field corresponds to the "Description-Content-Type" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-
+    long_description_content_type="text/markdown",  # Optional (see note above)
     # This should be a valid link to your project's main homepage.
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/mobalt/ccf_libs',  # Optional
-
-    author='Moises Baltazar Garcia',
-    author_email='moises@wustl.edu',
-
-
+    url="https://github.com/mobalt/ccf_libs",  # Optional
+    author="Moises Baltazar Garcia",
+    author_email="moises@wustl.edu",
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={'ccf': 'src'},  # Optional
-
+    package_dir={"ccf": "src"},  # Optional
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -80,21 +71,19 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    #packages=find_packages(where='src'),  # Required
+    # packages=find_packages(where='src'),  # Required
     packages=["ccf"],  # Required
-
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. If you
     # do not support Python 2, you can simplify this to '>=3.5' or similar, see
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires='>=3.5',
-
+    python_requires=">=3.5",
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pandas', 'boxsdk[jwt]', 'requests', 'PyYAML'],  # Optional
+    install_requires=["pandas", "boxsdk[jwt]", "requests", "PyYAML"],  # Optional
 )
